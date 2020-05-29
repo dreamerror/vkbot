a9d13a30ed23cef37cb4225d441521d5934a3812
dreamerror <66074257+dreamerror@users.noreply.github.com>
2020-05-29 15:20:23 +0900

    Add files via upload

--- bots/vkbot_group.py
+++ bots/vkbot_group.py
@@ -11,10 +11,10 @@ HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
                          'Chrome/81.0.4044.138 YaBrowser/20.4.2.201 Yowser/2.5 Yptp/1.23 Safari/537.36',
            'accept': '*/*'}
 
-login, password = '89243802946', 'trynottodie'
-vk_session = vk_api.VkApi(login=login, password=password, app_id=2685278)
+token = '93d91c4f8c5582aabdf897639a7231f62805e1e4a8bc6b8150351f42bff80be828fbd7f05b1809a44f857'
+vk_session = vk_api.VkApi(token=token)
 try:
-    vk_session.auth(token_only=True)
+    vk_session = vk_api.VkApi(token=token)
     print('Succesfully authorised')
 except vk_api.AuthError as error_msg:
     print(error_msg)
@@ -39,17 +39,15 @@ for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW:
         print('Сообщение пришло в: ', str(datetime.strftime(datetime.now(), '%H:%M:%S')))
         print('Текст сообщения: ' + str(event.text))
-        print('В диалоге с ' + get_user_name(event.user_id) + f', id пользователя = {event.user_id}')
+        print('Отправитель — {}, id пользователя — {}'.format(get_user_name(event.user_id), event.user_id))
         print('=' * 100)
         response = event.text.lower()
         if event.from_user and not event.from_me:
             if response == "12!":
                 vk_session.method('messages.send', {'user_id': event.user_id,
                                                     'message': 'И тебе привет от бота!', 'random_id': 0})
-            elif response == 'котейки':
-                attachment = get_pictures.get(vk_session, -175086907, session_api)
+            elif response == 'мемы':
+                attachment = get_pictures.get(vk_session, -142574308, session_api)
                 vk_session.method('messages.send', {'user_id': event.user_id,
                                                     'message': 'Лови котиков!', 'random_id': 0,
                                                     'attachment': attachment})
-
-
